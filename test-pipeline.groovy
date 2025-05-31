a8418dbdd4677043c40720b26765027bf7d19a87
itsdevopsprofile <146256531+itsdevopsprofile@users.noreply.github.com>
2025-05-31 20:06:13 +0530

    Create test-pipeline.groovy

---
+++ test-pipeline.groovy
@@ -0,0 +1,49 @@
+pipeline {
+    agent any
+    tools {
+        jdk 'jdk17'
+        nodejs 'node16'
+    }
+    environment {
+        SCANNER_HOME = tool 'sonar-scanner'
+    }
+    stages {
+        stage('clean workspace') {
+            steps {
+                cleanWs()
+            }
+        }
+        stage('Checkout from Git') {
+            steps {
+                git branch: 'main', url: 'https://github.com/itsdevopsprofile/jenkins-netflix-clone.git'
+            }
+        }
+        stage("Sonarqube Analysis") {
+            steps {
+                withSonarQubeEnv('sonar-server') {
+                    sh '''$SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=Netflix \
+                    -Dsonar.projectKey=Netflix'''
+                }
+            }
+        }
+        stage("quality gate") {
+            steps {
+                script {
+                    waitForQualityGate abortPipeline: false, credentialsId: 'sonar-token'
+                }
+            }
+        }
+        stage('Install Dependencies') {
+            steps {
+                sh "npm install"
+            }
+        }
+        
+        stage('Deploy'){
+            steps{
+                sh 'docker build --build-arg TMDB_V3_API_KEY=020581a34f3ab93b1360a55bea864bd9 -t netflix .'
+                sh 'docker run -d --name netflix -p 8081:80 netflix'
+            }
+        }
+    }
+}
